9363d07dea238e46823ee173f6398ad1f3da7c78
Bree Yates <byates13@uncc.edu>
2021-10-19 09:08:29 -0400

    flask app #2 - created a route and view to display all notes

--- flask_app/flask02.py
+++ flask_app/flask02.py
@@ -1,12 +1,12 @@
 # FLASK Tutorial 1 -- We show the bare bones code to get an app up and running
 
 # imports
-import os                 # os is used to get environment variables IP & PORT
-from flask import Flask   # Flask is the web app that we will customize
+import os  # os is used to get environment variables IP & PORT
+from flask import Flask  # Flask is the web app that we will customize
 from flask import render_template
 
+app = Flask(__name__)  # create an app
 
-app = Flask(__name__)     # create an app
 
 # @app.route is a decorator. It gives the function "index" special powers.
 # In this case it makes it so anyone going to "your-url/" makes this function
@@ -14,10 +14,18 @@ app = Flask(__name__)     # create an app
 @app.route('/index')
 def index():
     a_user = {'name': 'Bree', 'email': 'byates13@uncc.edu'}
-    return render_template('index.html', user = a_user)
+    return render_template('index.html', user=a_user)
 
 
-app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)
+@app.route('/notes')
+def get_notes():
+    notes = {1: {'title': 'First note', 'text': 'This is my first note', 'date': '10-18-2021'},
+             2: {'title': 'Second note', 'text': 'This is my second note', 'date': '10-19-2021'}
+             }
+    return render_template('notes.html', notes=notes)
+
+
+app.run(host=os.getenv('IP', '127.0.0.1'), port=int(os.getenv('PORT', 5000)), debug=True)
 
 # To see the web page in your web browser, go to the url,
 #   http://127.0.0.1:5000
